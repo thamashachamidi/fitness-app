a03c019b000d91ac329314205c6c94d65b05d5f8
Thamasha <thamashachamidi2@icloud.com>
2023-05-09 23:09:44 +0530

    created the next button for the onboarding screen

--- Ffitness app/ViewController.swift
+++ Ffitness app/ViewController.swift
@@ -8,12 +8,33 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    let nextButton = UIButton()
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        view.backgroundColor = .systemMint
+        setupButton()
+       
     
         // Do any additional setup after loading the view.
+    }
+    
+    func setupButton() {
+        view.addSubview(nextButton)
+        
+        nextButton.configuration = .filled()
+        nextButton.configuration?.baseBackgroundColor = .systemPink
+        nextButton.configuration?.title = "Next"
+        
+        nextButton.translatesAutoresizingMaskIntoConstraints = false
+        
+        NSLayoutConstraint.activate([
+            nextButton.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            nextButton.centerYAnchor.constraint(equalTo: view.centerYAnchor),
+            nextButton.widthAnchor.constraint(equalToConstant: 200),
+            nextButton.heightAnchor.constraint(equalToConstant: 50)
+        ]
+        )
     }
 
 
