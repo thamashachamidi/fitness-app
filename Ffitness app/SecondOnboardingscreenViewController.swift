bd9f607fa66071435acbea8a41b962f24200c71e
Thamasha <thamashachamidi2@icloud.com>
2023-05-13 23:06:23 +0530

    working on workout screen

--- Ffitness app/SecondOnboardingscreenViewController.swift
+++ Ffitness app/SecondOnboardingscreenViewController.swift
@@ -13,18 +13,59 @@ class SecondOnboardingscreenViewController: UIViewController {
         super.viewDidLoad()
         view.backgroundColor = .systemBackground
 
-        // Do any additional setup after loading the view.
+       
+        let titleLabel = UILabel()
+        titleLabel.text = "Work out anywhere"
+        titleLabel.font = UIFont.systemFont(ofSize: 28, weight: .bold)
+        titleLabel.numberOfLines = 0
+        titleLabel.textAlignment = .center
+
+        let descriptionLabel = UILabel()
+        descriptionLabel.text = "You can do your workout at home without any equiment, outside or at gym"
+        descriptionLabel.font = UIFont.systemFont(ofSize: 18, weight: .regular)
+        descriptionLabel.numberOfLines = 0
+        descriptionLabel.textAlignment = .center
+        
+        let button = UIButton()
+        button.setTitle("Skip", for: .normal)
+        button.backgroundColor = UIColor.blue
+        button.addTarget(self, action: #selector(gotoNextScreen), for: .touchUpInside)
+        button.translatesAutoresizingMaskIntoConstraints = false
+        view.addSubview(button)
+
+        
+        
+        let bottomConstraint = button.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor, constant: -16)
+        let leadingConstraint = button.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 16)
+        let trailingConstraint = button.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -16)
+        let heightConstraint = button.heightAnchor.constraint(equalToConstant: 44)
+
+        NSLayoutConstraint.activate([bottomConstraint, leadingConstraint, trailingConstraint, heightConstraint])
+
+
+        let stackView = UIStackView(arrangedSubviews: [titleLabel, descriptionLabel])
+        stackView.axis = .vertical
+        stackView.spacing = 20
+        stackView.translatesAutoresizingMaskIntoConstraints = false
+
+        view.addSubview(stackView)
+
+        NSLayoutConstraint.activate([
+            stackView.centerYAnchor.constraint(equalTo: view.centerYAnchor),
+            stackView.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 20),
+            stackView.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -20)
+        ])
+    
     }
     
 
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    
+    @objc func gotoNextScreen(){
+        let nextscreen = SecondOnboardingscreenViewController()
+        //nextscreen.title = "Back "
+        navigationController?.pushViewController(nextscreen, animated: true)
     }
-    */
+    
+
 
 }
