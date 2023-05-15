955e98a65529611a8043768d3045ebc87d3d8031
Thamasha <thamashachamidi2@icloud.com>
2023-05-15 23:25:29 +0530

    done few changes

--- Ffitness app/WorkoutListViewController.swift
+++ Ffitness app/WorkoutListViewController.swift
@@ -9,41 +9,11 @@ import UIKit
 
 class WorkoutListViewController: UIViewController {
     
-    var tableView = UITableView()
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .systemBackground
         
-        configureTableView()
-        
-        
-    }
-    
-    func configureTableView() {
-        view.addSubview(tableView)
-        setTableViewDelegates()
-        tableView.rowHeight = 100
-        
-        tableView.pin(to: view)
-    }
-    
-    
-    func setTableViewDelegates(){
-        tableView.delegate = self
-        tableView.dataSource = self
-    }
-}
-
-
-extension WorkoutListViewController:UITableViewDelegate,UITableViewDataSource {
-    
-    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        return UITableViewCell()
-    }
-    
-    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 10
-    }
-}
+    }   } 
 
